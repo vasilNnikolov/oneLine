6f87ffc3ff63245271969a7527b0c9029b85e65d
Vasil Nikolov <vaskonikolov2003@gmail.com>
2021-06-01 12:48:04 +0300

    added second screen, cannot clear objects

--- oneLineProject/main.py
+++ oneLineProject/main.py
@@ -18,7 +18,7 @@ class OneLineProgram:
         self.window.title("Choose an image file")
 
         # set controll buttons
-        self.set_next_button(command=None, enabled=False)
+        self.set_next_button(command=lambda: self.set_second_window(), enabled=False)
         self.set_back_button(command=self.window.destroy, enabled=True)
         tk.Button(self.window, text="Select Image", command=lambda: (self.get_filename(),
                                                                      self.set_next_button(enabled=self.verify_filename()),
@@ -26,8 +26,9 @@ class OneLineProgram:
                   ).place(x=0.4*self.size[0], y=0.8*self.size[1])
 
     def verify_filename(self):
-        if self.filename.split(".")[1] in self.allowed_images_types:
-            return True
+        if len(self.filename) > 0 and len(self.filename.split(".")) > 0:
+            if self.filename.split(".")[1] in self.allowed_images_types:
+                return True
         return False
 
     def get_filename(self):
@@ -63,23 +64,35 @@ class OneLineProgram:
 
     def set_canvas(self):
         if self.verify_filename():
-            print(f"valid filename: {self.filename}")
-            # set canvas showing image
             canvas_size = (int(0.6*self.size[0]), int(0.6*self.size[1]))
+
+            # open image, and resize it
+            pil_image = Image.open(self.filename)
+            w, h = pil_image.size
+            resize_horisontal = w/canvas_size[0]
+            resize_vertical = h/canvas_size[1]
+            resize = max(resize_horisontal, resize_vertical)
+            pil_image = pil_image.resize((int(w/resize), int(h/resize)))
+
+            # set canvas showing image
             image_canvas = tk.Canvas(self.window, width=canvas_size[0], height=canvas_size[1], bg="cyan")
-            image_canvas.place(x=0, y=0)
-            # pil_image = Image.open(self.filename)
-            pil_image = Image.open(self.filename).resize(canvas_size)
-            pil_image.show()
+            image_canvas.place(x=int(0.2*self.size[0]), y=50)
             img = ImageTk.PhotoImage(pil_image)
-            image_canvas.create_image(0, 0, anchor=tk.NW, image=img)
+            image_canvas.create_image(int(canvas_size[0]/2), int(canvas_size[1]/2), image=img)
             image_canvas.image = img
-
-
 
     def clear_window(self):
         for element in self.window.slaves():
             element.destroy()
+
+    def set_second_window(self):
+        self.clear_window()
+        self.window.title("Choose image parameters")
+
+        # set controll buttons
+        # self.set_next_button(command=None, enabled=False)
+        # self.set_back_button(command=self.set_start_screen, enabled=True)
+
 
 def main():
     program = OneLineProgram()
