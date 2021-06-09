ed4832ee311cbec137727143b27cdc6c9f6d2991
Vasil Nikolov <vaskonikolov2003@gmail.com>
2021-06-09 15:42:05 +0300

    square spiral working

--- oneLineProject/oneLineLibrary.py
+++ oneLineProject/oneLineLibrary.py
@@ -2,6 +2,8 @@ import math
 import tkinter as tk
 import random
 from PIL import Image, ImageDraw
+import matplotlib.pyplot as plt
+
 
 def make_picture_circular(image):
     w, h = image.size
@@ -38,17 +40,19 @@ def make_spiral(turns, image_size, start_point, end_point):
         angle_end, angle_start = angle_start, angle_end
         start_point, end_point = end_point, start_point
         print("swap")
-    print(angle_start, angle_end)
+    alpha1 = -angle_start
+    alpha2 = -angle_end + math.pi
     draw = ImageDraw.Draw(result)
     line_width = 3
     last_x_start, last_y_start = 0, 0
     last_x_end, last_y_end = 0, 0
+    dN = (alpha2 - alpha1)/2
     for i in range(N, 0, -1):
         t = i/N
         r = R*t
 
-        theta_start = turns*(1 - f(t)*t) - angle_start*s(t)
-        theta_end = turns*(1 - f(t)*t) - angle_end*s(t)
+        theta_start = (turns - dN)*(1 - f(t)*t) - alpha1
+        theta_end = (turns + dN)*(1 - f(t)*t) - alpha2
         center_start = (image_size[0]/2 + r*math.cos(theta_start), image_size[1]/2 - r*math.sin(theta_start))
         center_end = (image_size[0]/2 - r*math.cos(theta_end), image_size[1]/2 + r*math.sin(theta_end))
         if last_x_start != 0 and last_y_start != 0:
@@ -59,18 +63,63 @@ def make_spiral(turns, image_size, start_point, end_point):
 
     return result
 
+def stretch_distance(angle):
+    return (math.sin(angle)**4 + math.cos(angle)**4)**(-0.25)
+
+def make_square_spiral(turns, image_size, start_point, end_point):
+    R = min(image_size[0], image_size[1])/2
+    result = Image.new("RGB", image_size, "white")
+    N = 1000
+    angle_start = atan3(image_size[1]/2 - start_point[1], start_point[0] - image_size[0]/2)
+    angle_end = atan3(image_size[1]/2 - end_point[1], end_point[0] - image_size[0]/2)
+
+    if angle_end < angle_start:
+        angle_end, angle_start = angle_start, angle_end
+        start_point, end_point = end_point, start_point
+        print("swap")
+    alpha1 = -angle_start
+    alpha2 = -angle_end + math.pi
+    draw = ImageDraw.Draw(result)
+    last_x_start, last_y_start = 0, 0
+    last_x_end, last_y_end = 0, 0
+    dN = (alpha2 - alpha1)/2
+    for i in range(N, 0, -1):
+        t = i/N
+        theta_start = (turns - dN)*(1 - f(t)*t) - alpha1
+        theta_end = (turns + dN)*(1 - f(t)*t) - alpha2
+        r_start = R*t*stretch_distance(theta_start)
+        r_end = R*t*stretch_distance(theta_end)
+
+        center_start = (image_size[0]/2 + r_start*math.cos(theta_start), image_size[1]/2 - r_start*math.sin(theta_start))
+        center_end = (image_size[0]/2 - r_end*math.cos(theta_end), image_size[1]/2 + r_end*math.sin(theta_end))
+        if last_x_start != 0 and last_y_start != 0:
+            draw.line((center_start[0], center_start[1], last_x_start, last_y_start), fill="black")
+            draw.line((center_end[0], center_end[1], last_x_end, last_y_end), fill="black")
+        last_x_start, last_y_start = center_start
+        last_x_end, last_y_end = center_end
+
+    return result
+
+
 if __name__ == "__main__":
     image_size = (500, 500)
 
+    # start = (250, 0)
+    # end = (450, 150)
     start_angle = 2*random.random()*math.pi
     end_angle = 2*random.random()*math.pi
     start = (250 + 250*math.cos(start_angle), 250 + 250*math.sin(start_angle))
     end = (250 + 250*math.cos(end_angle), 250 + 250*math.sin(end_angle))
     print(start, end)
 
-    img = make_spiral(14, image_size, start, end)
+    img = make_square_spiral(18, image_size, start, end)
     draw = ImageDraw.Draw(img)
     draw.ellipse((start[0] - 5, start[1] - 5, start[0] + 5, start[1] + 5), fill="green")
     draw.ellipse((end[0] - 5, end[1] - 5, end[0] + 5, end[1] + 5), fill="red")
     img.show()
+
+    # xs = [i*2*math.pi/1000 for i in range(1000)]
+    # ys = [distance_to_center(x) for x in xs]
+    # plt.plot(xs, ys)
+    # plt.show()
 
